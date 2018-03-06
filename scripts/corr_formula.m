9648bea33bf40be355ef8f540961712555129a90
Carl Higgs <carlhiggs@gmail.com>
2018-03-06 22:02:20 +1100

    added in more admin files

---
+++ scripts/corr_formula.m
@@ -0,0 +1,17 @@
+
+
+% Inferences about  based on more than one sample
+% From: Burkett - Correlation_Regression_and_Analysis_of_Variance.
+r = [.7  .9]
+n = [19 25]
+z = atanh(r)
+meanzeta = z - 5*r/(2*n)
+varzeta = (n - 3/2 + (5/2)*(1-r.^2)).^-1
+postvz = (1/varzeta(1) + 1/varzeta(2))^-1
+postmz = postvz*sum(meanzeta./varzeta)
+rhohat = tanh(postmz)
+rhohat
+
+ci95zeta = [postmz - 1.96*sqrt(postvz)  postmz + 1.96*sqrt(postvz)]
+ci95rho  = [tanh(ci95zeta(1)) tanh(ci95zeta(2))]
+ci95rho
