556468d4efbd7c772bd87c9c192a47e2cbfe32ea
Carl Higgs <carlhiggs@gmail.com>
2018-03-06 22:12:09 +1100

    updates to make things neater

--- scripts/corr_formula.m
+++ scripts/corr_formula.m
@@ -1,7 +1,13 @@
+% Project: PowerMzDzDiffR - bca_rp2
+% Date: 28 Feb 2018
+% Author:  Carl Higgs
+% Purpose: Sketch file for matlab code to get a feel for the language
 
 
-% Inferences about  based on more than one sample
+% Inferences about \rho\hat based on more than one sample
 % From: Burkett - Correlation_Regression_and_Analysis_of_Variance.
+% http://www.academia.edu/34260699/Correlation_Regression_and_Analysis_of_Variance
+
 r = [.7  .9]
 n = [19 25]
 z = atanh(r)
