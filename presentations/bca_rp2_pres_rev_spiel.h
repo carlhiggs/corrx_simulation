0cf868713b44a842040d8527089e186d199032a1
Carl Higgs <carlhiggs@gmail.com>
2018-05-22 20:11:14 +1000

    adding in the spiel, i forgot to add before

---
+++ presentations/bca_rp2_pres_rev_spiel.h
@@ -0,0 +1,25 @@
+1. Research question
+My research project is concerned with power analysis of the detection of differences in correlations between idenetical and non-identical twin pairs, as a pre-cursor step to undertaking a classic twin study.
+
+The classic twin study exploits the differing degrees of genetic relatedness in identical twins and non-identical twins in order to draw inferences on the heritability of traits
+
+In broad terms, heritability is the degree to which variation in a trait or phenotype, such as propensity to gain body weight, or become a centenarian, can be attributed to genetics.
+
+Identical twins arise from the same zygote, or fertilised egg, and are genetically very similar.  Non-identical twins arise from fertilisation of two seperate eggs, and are as genetically alike as ordinary siblings.  
+
+2. 
+We might assume that variation in a trait occuring in identical twins must be due to environmental, not genetic factors. 
+If we make a some more assumptions, understanding that these likely do not strictly hold in practice we can undertake a classic twin study.  
+
+3. 
+the comparison of phenotypic traits within identical and non-identical twin pair samples allows for partitioning the variance in traits into that attributable to  shared environment, individual environment or to genetics.  This allows us to for example have a better understanding of the mechanics of health and disease processes so that we can develop intervention measures which appropriately target the hypothesised causal mechanisms.
+[see Falconer 183, 184]
+[also http://www.cureffi.org/2013/02/04/how-to-calculate-heritability/]
+[1 min 15 Secs]
+You will have noticed the clustered data structure here. 
+Contemporary twin studies use mixed effects and structural equation modelling evaluate these clustered differences in variance components for a particular trait between mono and dizgotic twins.  A recent article reported on the development of R commands which can be used to estimate the power to detect a difference in these.  
+
+However, a routine preliminary step undertaken by researchers in this field is the calculation and comparison of the more basic Pearson correlations as a precursor to considering intraclass correlations.  This research project focuses on the reported needs of researchers undertaking this early analysis step to consider the power to detect differences in Pearson correlations for bivariate normal and non-normally distributed data between mono and dizygotic twins.
+
+Methods
+4. 
